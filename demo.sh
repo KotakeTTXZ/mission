cb285931c7a7ba01d18db5792c0c474ee125f0a8
Kotake <ttxz123@gmail.com>
2018-03-11 00:18:28 +0800

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -6,7 +6,6 @@ else
  cd ./$1
  mkdir ./css
  mkdir ./js
- touch ./js/main.js
  echo -e  "<!DOCTYPE>\n<title>hello</title>\n<h1>Hi</h1>" > index.html
  echo "hi{color: red;}" > ./css/style.css
  echo -e 'var string = "Hello World"\nalert(string)' > ./js/main.js
