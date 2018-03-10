37ce1e4a4eb2243bd257a4d03dc486fac52b28fd
Kotake <ttxz123@gmail.com>
2018-03-11 00:16:37 +0800

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh sh
+if [ -d $1 ];then
+  echo "sorry:dir exists"
+else
+ mkdir ./$1
+ cd ./$1
+ mkdir ./css
+ mkdir ./js
+ touch ./js/main.js
+ echo -e  "<!DOCTYPE>\n<title>hello</title>\n<h1>Hi</h1>" > index.html
+ echo "hi{color: red;}" > ./css/style.css
+ echo -e 'var string = "Hello World"\nalert(string)' > ./js/main.js
+fi
